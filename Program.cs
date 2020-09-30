64a297e6b2b8d6f3be5c0300706660f428bd8818
matheussette <43152435+matheussette@users.noreply.github.com>
2020-09-30 18:12:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _0709
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double altura, largura,area,perimetro;
+
+            Console.WriteLine("Digite as medidads do retângulo:");
+            Console.WriteLine("Digite a altura:");
+            altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a largura");
+            largura = double.Parse(Console.ReadLine());
+            area = altura * largura;
+            Console.WriteLine("A área do retângulo é:");
+            Console.WriteLine(area);
+            perimetro = +((2 * largura) + (2 * altura));
+            Console.WriteLine("O perimetro do retângulo é:");
+            Console.WriteLine(perimetro);
+            Console.ReadKey();
+        }
+    }
+}
+ 
+
+
+
+
+
+
