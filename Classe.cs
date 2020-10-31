172bbdadaadd7644c41a612bf09579e27763f7ec
matheussette <43152435+matheussette@users.noreply.github.com>
2020-10-30 23:00:21 -0300

    Add files via upload

---
+++ Classe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Construtores
+{
+    class Classe
+    
+
+            {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
+
