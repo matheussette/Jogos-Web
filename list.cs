6c7aa5faa61061abaffc133af06285dfc0d56024
matheussette <43152435+matheussette@users.noreply.github.com>
2020-11-23 19:27:07 -0300

    Add files via upload

---
+++ list.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    class list
+    {
+        public static string Fábio { get; private set; }
+        public static string Wilson { get; private set; }
+        public static string Angelina { get; private set; }
+        public static string Eliney { get; private set; }
+        public static string Andre { get; private set; }
+        public static object Lucas { get; private set; }
+    }
+}
