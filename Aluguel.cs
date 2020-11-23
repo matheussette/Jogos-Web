fa639fea7a6607d4587fce124c4b89cb7b1a03fa
matheussette <43152435+matheussette@users.noreply.github.com>
2020-11-22 23:51:51 -0300

    Add files via upload

---
+++ Aluguel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetoresHotel
+{
+    class Aluguel
+    {
+        public string nome { get; set; } //encapsulamento
+        public string email { get; set; }// encapsulamento
+        public Aluguel(string nome, string email)
+        {
+            this.nome = nome; //  acessar um membro da instância da própria classe
+            this.email = email;// acessar o membro da instância da própria classe
+        }
+        public override string ToString()
+        {
+            return nome + ", " + email; //retorna a função main
+        }
+    }
+}
+            
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VetoresHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = int.Parse(Console.ReadLine());
+            Aluguel[] quartos = new Aluguel[10];
+            Console.Write("Quantos hóspedes serão registrados? ");// quantidade de hospedes a serem registrados
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine("Dados do " + i + "° hóspede: ");
+                Console.Write("Nome: ");//recebe os nomes
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");//recebe os emails
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");//recebe os quartos
+                int pos = int.Parse(Console.ReadLine());
+                quartos[pos] = new Aluguel(nome, email);
+            }
+            Console.WriteLine("Quartos ocupados:  ");
+            for (int i = 0; i < 10; i++) //enquanto i for igual a 0 e menor q 10 repete
+            {
+                if (quartos[i] != null) // se a condição for falsa executa a exibição das informações
+                {
+                    Console.WriteLine(i + ": " + quartos[i]); //exibe as informações dos quartos ocupados
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
