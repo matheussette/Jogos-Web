c12ba2b3ea8405e416d5da68ee48bf5b22e1304f
matheussette <43152435+matheussette@users.noreply.github.com>
2020-11-30 22:47:59 -0300

    Add files via upload

---
+++ ProdutoU.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Polimorfismo
+{
+    class ProdutoU :ProdutoC
+    {
+        public DateTime fabricacao; //variavel do tipo data
+        public ProdutoU(string Nome, double Preco, DateTime Fabricacao) : base(Nome, Preco) //parametros que a classe vai receber
+        {
+            fabricacao = Fabricacao;
+        }
+        public override string etiqueta() //Etiqueta do produto usado
+        {
+            return nome + " (usado) - R$ "
+                + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + "(fabricado: " + fabricacao.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture) + ")";
+        }
+    }
+}
